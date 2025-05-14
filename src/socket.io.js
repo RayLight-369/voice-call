94cd72fe3e67ab5fe624558cd54e4068aa9d5738
RayLight-369 <abdulrafay.designs@gmail.com>
2025-05-14 06:50:09 -0700

    update

--- src/socket.io.js
+++ src/socket.io.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
 
-export const socket = io( "http://localhost:3030", {
+export const socket = io( "https://video-call-server-vvga.onrender.com", {
   autoConnect: false
 } );
